6755e47a0d3b30dfe6ded5764bca535942a341f2
NickNameInvalid <zssmh98@outlook.com>
2024-04-11 18:08:51 -0400

    update esc-50 data processing

--- get_dataset_script/proc_esc.py
+++ get_dataset_script/proc_esc.py
@@ -1,54 +1,107 @@
-import torch
 import numpy as np
-import pickle
 import os
+import pandas as pd
+import librosa
+from tqdm import tqdm
+import torch
+from sklearn.utils import shuffle
 
-img_size=32
-classes={
-    'train': [1, 2,  3,  4, 5, 6, 9, 10, 15, 17, 18, 19],
-    'val':   [8, 11, 13, 16],
-    'test':  [0, 7,  12, 14]
-  }
 
-def _get_file_path(filename=""):
-  return os.path.join('./data', "ESC-50/", filename)
+audio_dir = './data/ESC-50/audio'
+metadata = './data/ESC-50/meta/esc50.csv'
+meta_df = pd.read_csv(metadata)
+meta_df = shuffle(meta_df)
+num_sample = len(meta_df)
 
-def _unpickle(filename):
-  """
-  Unpickle the given file and return the data.
-  Note that the appropriate dir-name is prepended the filename.
-  """
+ptr = 70
+pvl = 15
+pts = 15
 
-  # Create full path for the file.
-  file_path = _get_file_path(filename)
+def load_audio(file_path):
+  au, sr = librosa.load(os.path.join(audio_dir, file_path), sr=44100)
+  return au.reshape(-1, 1).T
 
-  print("Loading data: " + file_path)
+data = []
+label = []
+file = []
 
-  with open(file_path, mode='rb') as file:
-    # In Python 3.X it is important to set the encoding,
-    # otherwise an exception is raised here.
-    data = pickle.load(file, encoding='latin1')
+for row in tqdm(meta_df.itertuples(index=True, name='Pandas'), total=len(meta_df)):
+    data.append(load_audio(row[1]))
+    label.append(row[3])
+    file.append(row[1])
 
-  return data
+dtr = data[:num_sample * ptr]
+dvl = data[num_sample * ptr:num_sample * (ptr + pvl)]
+dts = data[num_sample * (ptr + pvl):]
 
-# import IPython
-# IPython.embed()
-meta  = _unpickle('meta')
-train = _unpickle('train')
-test  = _unpickle('test')
+ltr = label[:num_sample * ptr]
+lvl = label[num_sample * ptr:num_sample * (ptr + pvl)]
+lts = label[num_sample * (ptr + pvl):]
 
-data = np.concatenate([train['data'], test['data']])
-labels = np.array(train['fine_labels'] + test['fine_labels'])
-filts = np.array(train['coarse_labels'] + test['coarse_labels'])
+ftr = file[:num_sample * ptr]
+fvl = file[num_sample * ptr:num_sample * (ptr + pvl)]
+fts = file[num_sample * (ptr + pvl):]
 
-cifar_data = {}
-cifar_label = {}
-for k, v in classes.items():
-  data_filter = np.zeros_like(filts)
-  for i in v: data_filter += ( filts == i )
-  assert data_filter.max() == 1
+labels = {
+  "train": ltr, 
+  "val": lvl, 
+  "test": lts}
 
-  cifar_data[k] = data[data_filter == 1]
-  cifar_label[k] = labels[data_filter == 1]
+data = {  
+  "train": dtr, 
+  "val": dvl, 
+  "test": dts}
 
-torch.save({'data': cifar_data, 'label': cifar_label}, './data/cifar100.pth')
+file = {
+  "train": ftr, 
+  "val": fvl, 
+  "test": fts}
+
+torch.save({'data': data, 'label': labels, 'file': file}, './data/esc50.pth')
+
+# rs = np.random.RandomState(123)
+# rs.shuffle(classes)
+# num_train, num_val, num_test = [
+#     int(float(ratio)/np.sum(train_val_test_ratio)*len(classes))
+#     for ratio in train_val_test_ratio]
+
+# classes = {
+#     'train': classes[:num_train],
+#     'val': classes[num_train:num_train+num_val],
+#     'test': classes[num_train+num_val:]
+# }
+
+# if not os.path.exists(target_dir):
+#     os.makedirs(target_dir)
+# for k in classes.keys():
+#     target_dir_k = os.path.join(target_dir, k)
+#     if not os.path.exists(target_dir_k):
+#         os.makedirs(target_dir_k)
+#     cmd = ['mv'] + [os.path.join(source_dir, c) for c in classes[k]] + [target_dir_k]
+#     subprocess.call(cmd)
+
+# _ids = []
+
+# for root, dirnames, filenames in os.walk(target_dir):
+#     for filename in filenames:
+#         if filename.endswith(('.jpg', '.webp', '.JPEG', '.png', 'jpeg')):
+#             _ids.append(os.path.join(root, filename))
+
+# for path in _ids:
+#     try:
+#         img = imread(path)
+#     except:
+#         print(img)
+#     if len(img.shape) < 3:
+#         print(path)
+#         img = np.tile(np.expand_dims(img, axis=-1), [1, 1, 3])
+#         imwrite(path, img)
+#     else:
+#         if img.shape[-1] == 1:
+#             print(path)
+#             img = np.tile(img, [1, 1, 3])
+#             imwrite(path, img)
+
+# # resize images
+# cmd = ['python', 'get_dataset_script/resize_dataset.py', './data/bird']
+# subprocess.call(cmd)
