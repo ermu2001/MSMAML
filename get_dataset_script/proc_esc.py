ab01c32cc95a003600ca68d24ecabdb480d67d8e
NickNameInvalid <zssmh98@outlook.com>
2024-04-13 07:10:44 +0400

    add category in data

--- get_dataset_script/proc_esc.py
+++ get_dataset_script/proc_esc.py
@@ -24,11 +24,15 @@ def load_audio(file_path):
 data = []
 label = []
 file = []
+classmap = {}
 
 for row in tqdm(meta_df.itertuples(index=True, name='Pandas'), total=len(meta_df)):
     data.append(load_audio(row[1]))
     label.append(row[3])
     file.append(row[1])
+    classmap[int(row[3])] = row[4]
+    
+classmap = sorted(classmap.items())
     
 tr_split = int(num_sample * ptr)
 vl_split = int(num_sample * (ptr + pvl))
@@ -60,51 +64,4 @@ file = {
   "val": fvl, 
   "test": fts}
 
-torch.save({'data': data, 'label': labels, 'file': file}, './data/esc50.pth')
-
-# rs = np.random.RandomState(123)
-# rs.shuffle(classes)
-# num_train, num_val, num_test = [
-#     int(float(ratio)/np.sum(train_val_test_ratio)*len(classes))
-#     for ratio in train_val_test_ratio]
-
-# classes = {
-#     'train': classes[:num_train],
-#     'val': classes[num_train:num_train+num_val],
-#     'test': classes[num_train+num_val:]
-# }
-
-# if not os.path.exists(target_dir):
-#     os.makedirs(target_dir)
-# for k in classes.keys():
-#     target_dir_k = os.path.join(target_dir, k)
-#     if not os.path.exists(target_dir_k):
-#         os.makedirs(target_dir_k)
-#     cmd = ['mv'] + [os.path.join(source_dir, c) for c in classes[k]] + [target_dir_k]
-#     subprocess.call(cmd)
-
-# _ids = []
-
-# for root, dirnames, filenames in os.walk(target_dir):
-#     for filename in filenames:
-#         if filename.endswith(('.jpg', '.webp', '.JPEG', '.png', 'jpeg')):
-#             _ids.append(os.path.join(root, filename))
-
-# for path in _ids:
-#     try:
-#         img = imread(path)
-#     except:
-#         print(img)
-#     if len(img.shape) < 3:
-#         print(path)
-#         img = np.tile(np.expand_dims(img, axis=-1), [1, 1, 3])
-#         imwrite(path, img)
-#     else:
-#         if img.shape[-1] == 1:
-#             print(path)
-#             img = np.tile(img, [1, 1, 3])
-#             imwrite(path, img)
-
-# # resize images
-# cmd = ['python', 'get_dataset_script/resize_dataset.py', './data/bird']
-# subprocess.call(cmd)
+torch.save({'data': data, 'label': labels, 'file': file, 'class': classmap}, './data/esc50.pth')
